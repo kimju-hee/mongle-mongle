0c445ca803726060dbc92c1630931520b36d3552
김주희 <lsj240606@naver.com>
2025-06-17 11:10:16 +0900

    init : 프로젝트 초기 세팅

---
+++ mongle/src/main/java/diary/wep/mongle/MongleApplication.java
@@ -0,0 +1,13 @@
+package diary.wep.mongle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongleApplication.class, args);
+	}
+
+}
---
+++ mongle/src/test/java/diary/wep/mongle/MongleApplicationTests.java
@@ -0,0 +1,13 @@
+package diary.wep.mongle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
