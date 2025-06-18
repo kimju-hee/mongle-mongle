8aa76585037ba7767a4997f613fb4b7cb13922ff
김주희 <lsj240606@naver.com>
2025-06-18 17:22:16 +0900

    fix : 뷰 수정

--- mongle/src/main/java/diary/wep/mongle/controller/MainController.java
+++ mongle/src/main/java/diary/wep/mongle/controller/MainController.java
@@ -45,6 +45,23 @@ public class MainController {
         return "diary";
     }
 
+    @GetMapping("/mypage")
+    public String mypage(Model model, OAuth2AuthenticationToken authentication) {
+        if (authentication == null) return "redirect:/login-page";
+
+        OAuth2User oAuth2User = authentication.getPrincipal();
+        Map<String, Object> kakaoAccount = (Map<String, Object>) oAuth2User.getAttributes().get("kakao_account");
+        Map<String, Object> profile = (Map<String, Object>) kakaoAccount.get("profile");
+
+        String email = (String) kakaoAccount.get("email");
+        String nickname = (String) profile.get("nickname");
+
+        model.addAttribute("email", email != null ? email : "카카오 로그인");
+        model.addAttribute("nickname", nickname);
+
+        return "mypage";
+    }
+
 
     @GetMapping("/login-page")
     public String loginPage() {
